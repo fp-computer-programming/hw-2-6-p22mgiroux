61e1783a25e449f222263c3a3fc7bec7225bbf80
p22mgiroux <77790842+p22mgiroux@users.noreply.github.com>
2021-09-21 10:56:53 -0400

    Fix HW 2-6-1

--- hw2-6-1.py
+++ hw2-6-1.py
@@ -1,7 +1,7 @@
 # Author: MOG 9/21/21
 
-f_temp = input("What is the temperature in fahrenheit? ")
+f_temp = input("What is the temperature in fahrenheit?")
 
-c_temp = (f_temp * (9 / 5)) + 32
+c_temp = (int(f_temp) - 32) * (5 / 9)
 
-print("{} degrees in fahrenheit is equal to {} degrees in celcius.".format(f_temp, c_temp)
+print("{} degrees fahrenheit is equal to {} degrees celcius.".format(f_temp, c_temp))
