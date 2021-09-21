e89250630fe6933084a00a464e39f9a3c325134a
p22mgiroux <77790842+p22mgiroux@users.noreply.github.com>
2021-09-21 10:43:23 -0400

    Finish HW 6-1-1

---
+++ hw2-6-1.py
@@ -0,0 +1,7 @@
+# Author: MOG 9/21/21
+
+f_temp = input("What is the temperature in fahrenheit? ")
+
+c_temp = (f_temp * (9 / 5)) + 32
+
+print("{} degrees in fahrenheit is equal to {} degrees in celcius.".format(f_temp, c_temp)
