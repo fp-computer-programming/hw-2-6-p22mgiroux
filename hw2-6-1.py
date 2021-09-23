8279b066ddb11abe502b11e2409fb9b2f864094d
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-09-23 10:24:22 -0400

    Finish HW-6

--- hw2-6-1.py
+++ hw2-6-1.py
@@ -1,7 +1,12 @@
 # Author: MOG 9/21/21
 
-f_temp = input("What is the temperature in fahrenheit?")
+f_temp = input("What is the temperature in fahrenheit? ")
 
 c_temp = (int(f_temp) - 32) * (5 / 9)
 
-print("{} degrees fahrenheit is equal to {} degrees celcius.".format(f_temp, c_temp))
+fs = "s"
+if(f_temp == "1"): fs = ""
+cs = "s"
+if(c_temp == 1): fs = ""
+
+print("{} degree{} fahrenheit is equal to {} degree{} celcius.".format(f_temp, fs, c_temp, cs))
---
+++ hw2-6-2.py
@@ -0,0 +1,12 @@
+# Author: MOG 9/23/21
+
+pi = 3.14159
+
+rad = input("What is the radius of your cylinder? ")
+height = input("What is the height of your cylinder? ")
+
+volume = pi * int(rad) ** 2 * int(height)
+surf_area = 2 * pi * int(rad) * (int(height) + int(rad))
+
+print("Volume: {}".format(volume))
+print("Surface Area: {}".format(surf_area))
